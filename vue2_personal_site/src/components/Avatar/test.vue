822195e9926f2d96c1ef7c00b6e994dd101260d0
AliceVSCthulhu <504677170@qq.com>
2026-03-28 04:58:55 +0800

    feat: 新增头像组件 (#2)
    - 添加了头像组件。
    - 添加了头像组件测试test文件。
    - 在App.vue中进行了头像组件测试。

--- vue2_personal_site/src/components/Avatar/test.vue
+++ vue2_personal_site/src/components/Avatar/test.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
     <Avatar url="https://img1.baidu.com/it/u=2659077985,44740634&fm=253&fmt=auto&app=120&f=JPEG?w=889&h=500"/>
+    <Avatar url="https://img2.baidu.com/it/u=2334080515,397633521&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500"/>
   </div>
 </template>
 
 <script>
-import Avatar from './components/Avatar';
+import Avatar from './index.vue';
 export default {
   components: {
     Avatar
@@ -13,4 +14,4 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="less" scoped></style>
