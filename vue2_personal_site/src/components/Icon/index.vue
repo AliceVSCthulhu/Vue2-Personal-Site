0ea5bb056f39a51dd52518a6385defc95e9e013a
AliceVSCthulhu <504677170@qq.com>
2026-03-28 18:30:58 +0800

    feat: 新增Icon组件 (#3)
    - 添加了Icon组件。
    - 添加了Icon名称对象文件。
    - 添加了test测试文件。

---
+++ vue2_personal_site/src/components/Icon/index.vue
@@ -0,0 +1,30 @@
+<template>
+    <i class="iconfont icon-container" :class="curIconName"></i>
+</template>
+
+<script>
+import iconNames from "./iconName.js"
+export default {
+    name: "Icon",
+    props: {
+        type: {
+            type: String,
+            required: true
+        }
+    },
+    computed: {
+        curIconName() {
+            return iconNames[this.type]
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+@import "//at.alicdn.com/t/c/font_5149747_y8tb1ucuiki.css";
+
+.iconfont {
+    font-size: inherit;
+    color: inherit;
+}
+</style>
