51fe0ffcffeccc2fb783a8a4a0b0699a3eaa20ea
AliceVSCthulhu <504677170@qq.com>
2026-03-28 18:30:58 +0800

    feat: 新增Icon组件 (#3)
    - 添加了Icon组件。
    - 添加了Icon名称对象文件。
    - 添加了test测试文件。

---
+++ vue2_personal_site/src/components/Icon/test.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Icon type="empty"/>
+    <Icon type="home"/>
+  </div>
+</template>
+
+<script>
+import Icon from './index.vue';
+export default {
+  components: {
+    Icon
+  }
+}
+</script>
+
+<style lang="less" scoped></style>
