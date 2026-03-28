89c4cd150e82d1f2e707ecc35dc38402661e0abf
AliceVSCthulhu <504677170@qq.com>
2026-03-28 18:14:01 +0800

    chore: App新增Icon组件测试；新增Avatar组件名称

--- vue2_personal_site/src/App.vue
+++ vue2_personal_site/src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <Avatar url="https://img1.baidu.com/it/u=2659077985,44740634&fm=253&fmt=auto&app=120&f=JPEG?w=889&h=500"/>
+    <Icon type="weixin"/>
   </div>
 </template>
 
 <script>
 import Avatar from './components/Avatar';
+import Icon from "./components/Icon"
 export default {
   components: {
-    Avatar
+    Avatar,
+    Icon
   }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="less" scoped></style>
