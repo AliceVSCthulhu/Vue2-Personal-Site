c3a15c1edf2aab8c90fec03d303dd03c9abc56fa
AliceVSCthulhu <504677170@qq.com>
2026-03-30 20:49:41 +0800

    feat: 新增Empty组件

---
+++ vue2_personal_site/src/components/Empty/index.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="empty-container">
+        <Icon :type="iconType" />
+        <span>{{ text }}</span>
+    </div>
+</template>
+
+<script>
+import Icon from "../Icon/index";
+export default {
+    name: "Empty",
+    data() {
+        return {
+            iconType: "empty",
+        }
+    },
+    components: {
+        Icon,
+    },
+    props: {
+        text: {
+            type: String,
+            default: "无数据",
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+@color: rgb(175, 175, 175);
+
+.empty-container {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: @color;
+
+    .iconfont {
+        font-size: 10em;
+    }
+
+    span {
+        font-size: 1.3em;
+        font-family: 'heiti';
+    }
+}
+</style>
